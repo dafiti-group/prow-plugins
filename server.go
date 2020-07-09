34dde6a27decfd53dfd27e74c1e5853beb46f93d
yuri <yurifl03@gmail.com>
2020-07-09 15:55:06 -0300

    feature: remove url

--- server.go
+++ server.go
@@ -3,7 +3,7 @@ Copyright 2017 The Kubernetes Authors.
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
 You may obtain a copy of the License at
-    http://www.apache.org/licenses/LICENSE-2.0
+	http://www.apache.org/licenses/LICENSE-2.0
 Unless required by applicable law or agreed to in writing, software
 distributed under the License is distributed on an "AS IS" BASIS,
 WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -16,34 +16,15 @@ package main
 import (
 	"encoding/json"
 	"fmt"
-	"io/ioutil"
 	"net/http"
 	"regexp"
 
-	"github.com/k0kubun/pp"
 	"github.com/sirupsen/logrus"
 
 	"k8s.io/test-infra/prow/config"
 	"k8s.io/test-infra/prow/github"
 	"k8s.io/test-infra/prow/pluginhelp"
 )
-
-const pluginName = "refresh"
-
-var refreshRe = regexp.MustCompile(`(?mi)^/refresh\s*$`)
-
-func HelpProvider(_ []config.OrgRepo) (*pluginhelp.PluginHelp, error) {
-	pluginHelp := &pluginhelp.PluginHelp{
-		Description: `The refresh plugin is used for refreshing status contexts in PRs. Useful in case GitHub breaks down.`,
-	}
-	pluginHelp.AddCommand(pluginhelp.Command{
-		Usage:       "/refresh",
-		Description: "Refresh status contexts on a PR.",
-		WhoCanUse:   "Anyone",
-		Examples:    []string{"/refresh"},
-	})
-	return pluginHelp, nil
-}
 
 type Server struct {
 	tokenGenerator func() []byte
@@ -53,103 +34,123 @@ type Server struct {
 	log            *logrus.Entry
 }
 
+const (
+	InvalidLabel = "do-not-merge/no-jira-issue-on-title"
+)
+
+var (
+	titleRegex = regexp.MustCompile(`[A-Z]{1,}-\d+`)
+)
+
 func (s *Server) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-
-	requestBody, err := ioutil.ReadAll(r.Body)
-
-	logrus.Println("Request:", string(requestBody), err)
 	eventType, eventGUID, payload, ok, _ := github.ValidateWebhook(w, r, s.tokenGenerator)
 	if !ok {
+		s.log.Error("validate webhook failed")
 		return
 	}
-	fmt.Fprint(w, "Event received. Have a nice day.")
 
+	// Respond with
 	if err := s.handleEvent(eventType, eventGUID, payload); err != nil {
-		logrus.WithError(err).Error("Error parsing event.")
+		s.log.WithError(err).Error("Error parsing event.")
+		fmt.Fprint(w, "Something went wrong")
+		return
 	}
+
+	s.log.Info("handle event ok")
+	fmt.Fprint(w, "Event received. Have a nice day.")
 }
 
 func (s *Server) handleEvent(eventType, eventGUID string, payload []byte) (err error) {
-	l := logrus.WithFields(
+	l := s.log.WithFields(
 		logrus.Fields{
 			"event-type":     eventType,
 			github.EventGUID: eventGUID,
 		},
 	)
 
-	pp.Println(eventType)
-	pp.Println(eventGUID)
 	switch eventType {
 	case "pull_request":
-
 		var p github.PullRequestEvent
-		// var a1 github.PullRequest
-		// var a2 gogh.PullRequestEvent
-		// var a3 gogh.PullRequest
-
-		// err = json.Unmarshal(payload, &p)
-		// pp.Println("github.PullRequestEvent", p, "error", err)
-		// pp.Println(err)
-		// err = json.Unmarshal(payload, &a1)
-		// pp.Println("github.PullRequest", a1, "error", err)
-		// pp.Println(err)
-		// err = json.Unmarshal(payload, &a2)
-		// pp.Println("gogh.PullRequestEvent", a2, "error", err)
-		// pp.Println(err)
-		// err = json.Unmarshal(payload, &a3)
-		// pp.Println("gogh.PullRequest", a3, "error", err)
-		// pp.Println(err)
 
 		if err := json.Unmarshal(payload, &p); err != nil {
 			return err
 		}
 
-		if err := s.handlePR(l, &p); err != nil {
-			s.log.WithError(err).WithFields(l.Data).Info("Refreshing github statuses failed.")
-		}
-
-		// go func() {
-		// 	if err := s.handlePR(l, &p); err != nil {
-		// 		s.log.WithError(err).WithFields(l.Data).Info("Refreshing github statuses failed.")
-		// 	}
-		// }()
+		go func() {
+			if err := s.handlePR(l, &p); err != nil {
+				s.log.WithError(err).WithFields(l.Data).Info("Refreshing github statuses failed.")
+			}
+		}()
 	default:
-		logrus.Debugf("skipping event of type %q", eventType)
+		s.log.Debugf("skipping event of type %q", eventType)
 	}
 	return nil
 }
 
 func (s *Server) handlePR(l *logrus.Entry, p *github.PullRequestEvent) (err error) {
-	pp.Println(p)
 	var (
 		org    = p.Repo.Owner.Login
 		repo   = p.Repo.Name
 		number = p.Number
 		title  = p.PullRequest.Title
+		action = p.Action
+		msg    = "This pull request does not have a jira tag on the title"
 	)
 
-	pp.Println("title", title, "org", org, "repo", repo, "number", number)
-
+	// Setup Logger
 	l = l.WithFields(logrus.Fields{
 		github.OrgLogField:  org,
 		github.RepoLogField: repo,
 		github.PrLogField:   number,
 		"title":             title,
 	})
-	labels, err := s.ghc.GetIssueLabels(org, repo, number)
-	if err != nil {
-		l.Error(err)
-		return err
-	}
-	pp.Println(labels)
 
-	if title == "test" {
-		err = s.ghc.AddLabel(org, repo, number, "invalid")
-	}
-	if err != nil {
-		l.Error(err)
-		return err
+	l.Info("Handle PR")
+
+	// Only consider newly merged PRs
+	if action == github.PullRequestActionClosed {
+		l.Infof("Pull Request Action '%v' not aplicable", p.Action)
+		return nil
 	}
 
+	// Only add for one repo for now
+	if repo != "prow-plugins" {
+		l.Infof("Repo not '%v' not allowed", repo)
+		return nil
+	}
+
+	jiraTag := titleRegex.FindString(title)
+
+	if jiraTag == "" {
+		err = s.ghc.AddLabel(org, repo, number, InvalidLabel)
+		if err != nil {
+			l.WithError(err).Error("failed to add label")
+			return err
+		}
+
+		s.ghc.CreateComment(org, repo, number, msg)
+		if err != nil {
+			l.WithError(err).Error("failed to add label")
+			return err
+		}
+		return nil
+	}
+
+	// @TODO: Check Jira
+
+	err = s.ghc.RemoveLabel(org, repo, number, InvalidLabel)
+	if err != nil {
+		l.WithError(err).Error("failed to remove label")
+		return err
+	}
+	// cp.PruneComments(func(ic github.IssueComment) bool {
+	// 	return strings.Contains(ic.Body, blockedPathsBody)
+	// })
 	return err
+}
+
+func HelpProvider(_ []config.OrgRepo) (*pluginhelp.PluginHelp, error) {
+	return &pluginhelp.PluginHelp{
+		Description: "The Jira checker plugin checks your PR name",
+	}, nil
 }
